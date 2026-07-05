fcfb109f4063b0f5511dddae28c0a09e9a4db7e7
Pradip Ravindra Chavan <98585259+Pradip2602@users.noreply.github.com>
2026-07-05 14:04:01 +0000

    changes done

--- vars/push_dockerhub.groovy
+++ vars/push_dockerhub.groovy
@@ -1,8 +1,8 @@
 def call(String credId,String imageName){
     withCredentials([usernamePassword(
-                    credentialsId: '${credId}', 
-                    passwordVariable: 'dockerHubPass', 
-                    usernameVariable: 'dockerHubUser')]){
+                    credentialsId: "${credId}", 
+                    passwordVariable: "dockerHubPass", 
+                    usernameVariable: "dockerHubUser")]){
                         sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
                         echo "Docker login successful...."
                         sh "docker image tag ${imageName}:latest ${env.dockerHubUser}/${imageName}:latest"
